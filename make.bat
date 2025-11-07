6432a94e38528d32209f3edc6a0061ff99730821
Pep <socialkuma@hotmail.com>
2025-11-07 09:31:26 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,8 +1,10 @@
 @echo off
 set P=mstrdup.exe
+del %P%
 tclkitsh.exe sdx.kit wrap mdup -runtime tclkit.exe
 move /y mdup %P%
 echo.
 echo you can copy the file %P% to the destination folder of your choice
 echo copy mdup.cfg file to your home folder and change passwords properly
       
+
